abf7d6adba38a92f005dde063dbd60cd3a5deeef
Preety <pc15021992@gmail.com>
2022-02-10 18:56:18 +0530

    episode 5 - design single idea page

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,13 +1,13 @@
 <x-app-layout>
-   <div>
+    <div>
        <a href="/" class="flex items-center font-semibold hover:underline">
            <svg class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
             </svg>
            <span class="ml-2">All Ideas</span>
        </a>
-   </div>
-   <div class="idea-container bg-white rounded-xl flex mt-4">
+    </div>
+    <div class="idea-container bg-white rounded-xl flex mt-4">
        <div class="flex flex-1 px-4 py-8">
             <div class="flex-none">
                 <a href="#" >
@@ -18,7 +18,7 @@
                 <h4 class="text-xl font-semibold">
                     <a href="#" class="hover:underline">A random title can go here</a>
                 </h4>
-                <div class="text-gray-600 mt-3 line-clamp-3">
+                <div class="text-gray-600 mt-3">
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                 </div>
                 <div class="flex items-center justify-between mt-6">
@@ -37,7 +37,7 @@
                             <svg width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
                                 <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
                             </svg>
-                            <ul class="absolute w-44 text-left font-semibold bg-white shadow-dialog rounded-xl py-3 ml-8">
+                            <ul class=" hidden absolute w-44 text-left font-semibold bg-white shadow-dialog rounded-xl py-3 ml-8">
                                 <li><a href="#" class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Mark as spam</a></li>
                                 <li><a href="#" class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Delete Post</a></li>
                             </ul>
@@ -46,7 +46,7 @@
                 </div>
             </div>
        </div>
-    </div>
+    </div> <!-- end idea-container -->
     <div class="buttons-container flex items-center justify-between mt-6">
         <div class="flex items-center space-x-4 ml-6">
             <button
@@ -64,7 +64,123 @@
             </button>
         </div>
         <div class="flex items-center space-x-3">
-
+            <div class="bg-white font-semibold text-center rounded-xl px-3 py-2">
+                <div class="text-xl leading-snug">12</div>
+                <div class="text-gray-400 text-xs leading-none">Votes</div>
+            </div>
+            <button
+                type="button"
+                class="w-32 h-11 text-xs bg-gray-200 font-semibold uppercase rounded-xl border border-gray-200  hover:border-gray-400 transition duration-150 ease-in px-6 py-3">
+                <span>Vote</span>
+            </button>
         </div>
-    </div>
+    </div> <!-- end buttons-container -->
+    <div class="comments-container relative space-y-6 ml-22 pt-4 my-8 mt-1">
+        <div class=" is-admin comment-container relative bg-white rounded-xl flex mt-4">
+           <div class="flex flex-1 px-4 py-8">
+                <div class="flex-none">
+                    <a href="#" >
+                        <img src="https://www.facebeautyscience.com/wp-content/uploads/2020/04/face-beauty-skin-face2-proc.jpg" alt="avatar" class="w-10 h-10 rounded-xl">
+                    </a>
+                    <div class="text-center uppercase text-xxs font-bold text-blue mt-1">Admin</div>
+                </div>
+                <div class="w-full mx-4">
+                    <h4 class="text-xl font-semibold">
+                            <a href="#" class="hover:underline">Status changed to under consideration</a>
+                    </h4>
+                    <div class="text-gray-600 mt-3 line-clamp-3">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    </div>
+                    <div class="flex items-center justify-between mt-6">
+                        <div class="flex items-center text-xs text-gray-400 font-semibold space-x-4">
+                            <div class="font-bold text-blue">Andrea</div>
+                            <div>&bull;</div>
+                            <div>10 hours ago</div>
+                        </div>
+                        <div class="flex items-center space-x-2">
+                            <button class="relative bg-gray-100 hover:bg-gray-200 rounded-full h-7 transition duration-150 ease-in py-2 px-3">
+                                <svg width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
+                                    <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
+                                </svg>
+                                <ul class=" hidden absolute w-44 text-left font-semibold bg-white shadow-dialog rounded-xl py-3 ml-8">
+                                    <li><a href="#" class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Mark as spam</a></li>
+                                    <li><a href="#" class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Delete Post</a></li>
+                                </ul>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+           </div>
+        </div> <!-- end comment-container -->
+        <div class="comment-container relative bg-white rounded-xl flex mt-4">
+           <div class="flex flex-1 px-4 py-8">
+                <div class="flex-none">
+                    <a href="#" >
+                        <img src="https://www.facebeautyscience.com/wp-content/uploads/2020/04/face-beauty-skin-face2-proc.jpg" alt="avatar" class="w-10 h-10 rounded-xl">
+                    </a>
+                </div>
+                <div class="w-full mx-4">
+                    <!-- <h4 class="text-xl font-semibold">
+                            <a href="#" class="hover:underline">A random title can go here</a>
+                    </h4> -->
+                    <div class="text-gray-600 mt-3 line-clamp-3">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    </div>
+                    <div class="flex items-center justify-between mt-6">
+                        <div class="flex items-center text-xs text-gray-400 font-semibold space-x-4">
+                            <div class="font-bold text-gray-900">Philip</div>
+                            <div>&bull;</div>
+                            <div>10 hours ago</div>
+                        </div>
+                        <div class="flex items-center space-x-2">
+                            <button class="relative bg-gray-100 hover:bg-gray-200 rounded-full h-7 transition duration-150 ease-in py-2 px-3">
+                                <svg width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
+                                    <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
+                                </svg>
+                                <ul class=" hidden absolute w-44 text-left font-semibold bg-white shadow-dialog rounded-xl py-3 ml-8">
+                                    <li><a href="#" class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Mark as spam</a></li>
+                                    <li><a href="#" class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Delete Post</a></li>
+                                </ul>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+           </div>
+        </div> <!-- end comment-container -->
+        <div class="comment-container relative bg-white rounded-xl flex mt-4">
+           <div class="flex flex-1 px-4 py-8">
+                <div class="flex-none">
+                    <a href="#" >
+                        <img src="https://www.facebeautyscience.com/wp-content/uploads/2020/04/face-beauty-skin-face2-proc.jpg" alt="avatar" class="w-10 h-10 rounded-xl">
+                    </a>
+                </div>
+                <div class="w-full mx-4">
+                    <!-- <h4 class="text-xl font-semibold">
+                            <a href="#" class="hover:underline">A random title can go here</a>
+                    </h4> -->
+                    <div class="text-gray-600 mt-3 line-clamp-3">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    </div>
+                    <div class="flex items-center justify-between mt-6">
+                        <div class="flex items-center text-xs text-gray-400 font-semibold space-x-4">
+                            <div class="font-bold text-gray-900">Smith</div>
+                            <div>&bull;</div>
+                            <div>10 hours ago</div>
+                        </div>
+                        <div class="flex items-center space-x-2">
+                            <button class="relative bg-gray-100 hover:bg-gray-200 rounded-full h-7 transition duration-150 ease-in py-2 px-3">
+                                <svg width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
+                                    <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
+                                </svg>
+                                <ul class=" hidden absolute w-44 text-left font-semibold bg-white shadow-dialog rounded-xl py-3 ml-8">
+                                    <li><a href="#" class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Mark as spam</a></li>
+                                    <li><a href="#" class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Delete Post</a></li>
+                                </ul>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+           </div>
+        </div> <!-- end comment-container -->
+    </div> <!-- end comments-container -->
 </x-app-layout>
